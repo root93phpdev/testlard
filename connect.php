2c1b99cce00462179bf757b2635d6265c7e45fc1
v.derzhanskiy <root93@inbox.ru>
2024-06-28 14:40:58 +0300

    Create project

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+include('config.php');
+// Create connection
+$link = mysqli_connect($host, $login_db, $password_db, $name_db);
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ТЕСТ ЛАРД</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">LARD [тест]</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+      </div>
+    </nav>
+
+    <div class="container my-5">
+      <h1>Astrobotic создала лунный полигон для подготовки к полётам на Луну</h1>
+      <div class="col-lg-10 px-0">
+        <p class="fs-5"><b>Astrobotic продолжает развивать свои возможности в области космических технологий после запуска первого коммерческого лунного модуля Peregrine</b></p>
+        <p class="fs-5">Astrobotic представила лунный испытательный полигон, который поможет подготовиться к ряду предстоящих лунных миссий.</p>
+        <p class="fs-5">Лунный испытательный полигон (LSPG) размером примерно 330 на 330 футов (100 на 100 метров) в Мохаве (Калифорния), представляет собой высокоточное 3D-испытательное поле, которое воспроизводит условия на Луне.</p>
+        <p class="fs-5">LSPG, смоделированный с использованием инструментов LunaRay от Astrobotic, имитирует топографию Луны и экстремальные условия освещения на южном полюсе Луны. Он будет использоваться для тестирования технологий точной посадки на Луну, таких как сканеры LiDAR, а также для управления марсоходами и другими роботизированными системами.</p>
+        <hr class="col-4 my-4">
+        <div class="mb-3">
+  <input type="email" class="form-control fs-5" id="exampleFormControlInput1" placeholder="Укажите имя :)">
+</div>
+<div class="mb-3">
+  <textarea class="form-control fs-5" id="exampleFormControlTextarea1" rows="3" placeholder="Напишите всё, что хотели сказать =)"></textarea>
+
+</div>
+<button type="button" class="btn btn-primary btn-sm">Добавить комментарий <span id="btnspin"><i class="fa fa-arrow-circle-down" aria-hidden="true"></i></span></button>
+      <hr class="col-4 my-4">  
+    
+    
+    </div>
+    </div>
+
+    <script src="https://kit.fontawesome.com/abbef8312a.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+  </body>
+</html>
